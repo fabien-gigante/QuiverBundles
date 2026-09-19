2c3c258e1695db7936b19b52c6dc0f92fdf1a465
fabien-gigante <fabien.gigante@free.fr>
2026-09-19 17:30:44 +0200

    refactor projectile handling

--- src/main/java/com/fabien_gigante/quiver_bundles/mixin/PlayerMixin.java
+++ src/main/java/com/fabien_gigante/quiver_bundles/mixin/PlayerMixin.java
@@ -3,12 +3,14 @@ package com.fabien_gigante.quiver_bundles.mixin;
 import java.util.List;
 import java.util.function.Predicate;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.fabien_gigante.quiver_bundles.BundleSlot;
+import com.fabien_gigante.quiver_bundles.QuiverHolder;
 
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -22,16 +24,26 @@ import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.item.component.BundleContents;
 
 @Mixin(Player.class)
-public abstract class PlayerMixin {
+public abstract class PlayerMixin implements QuiverHolder {
 	@Shadow
 	public abstract Inventory getInventory();
+	@Unique
+	private @Nullable QuiverSlot currentQuiverSlot;
+
+    public @Override QuiverSlot getCurrentQuiverSlot() { return currentQuiverSlot; }
+    public @Override void setCurrentQuiverSlot(QuiverSlot slot) { this.currentQuiverSlot = slot; }
+
+	@Inject(method = "getProjectile", at = @At(value = "HEAD"))
+	private void clearQuiverProjectile(ItemStack weapon, CallbackInfoReturnable<ItemStack> cir) {
+		clearCurrentQuiverSlot();
+	}
 
     @Inject(
         method = "getProjectile",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;hasInfiniteMaterials()Z"),
         cancellable = true
     )
-	private void getProjectileFromBundles(ItemStack weapon, CallbackInfoReturnable<ItemStack> cir) {
+	private void getQuiverProjectile(ItemStack weapon, CallbackInfoReturnable<ItemStack> cir) {
 		Predicate<ItemStack> supportedProjectiles = ((ProjectileWeaponItem)weapon.getItem()).getAllSupportedProjectiles();
 		for (ItemStack stack : getInventory()) {
 			if (stack.isEmpty()) continue;
@@ -43,7 +55,7 @@ public abstract class PlayerMixin {
 				if (!inside.is(ItemTags.ARROWS)) continue;
 				ItemStack projectile = inside.create();
 				if (!supportedProjectiles.test(projectile)) continue;
-				projectile.set(BundleSlot.BUNDLE_SLOT, new BundleSlot(stack, j));
+				setCurrentQuiverSlot(projectile, stack, j);
 				cir.setReturnValue(projectile);
 				return;
 			}
